776e00767d30a9234a10b9de35c66dcdeb7917cf
Adgamov Artur <adgamoffartur@yandex.ru>
2023-05-26 16:41:10 +0300

    изменили урл авторизации

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = " https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.arturito.nomoredomains.rocks/";
 
 export function register(email, password) {
   return fetch(`${BASE_URL}/signup`, {
