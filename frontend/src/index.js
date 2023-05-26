c7145749c87709b7ed22dde7c9ee8ab41a227e47
Adgamov Artur <adgamoffartur@yandex.ru>
2023-05-26 15:38:30 +0300

    удалил базовый урл

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react-mesto-auth'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
