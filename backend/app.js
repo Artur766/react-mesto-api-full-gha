ee2289e0138c2338ab9b22b730b94279c4c70876
Adgamov Artur <adgamoffartur@yandex.ru>
2023-05-28 19:43:12 +0300

    Исправление ошибок от ревью 1.0

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
 
-app.use(express.json()); // для собирания JSON-формата
+app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
 // подключаемся к серверу mongo
